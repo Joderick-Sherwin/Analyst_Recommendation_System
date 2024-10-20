38eaea84545d97d5ff852dd0c5752619b3cbcc79
Joderick Sherwin J <130847427+Joderick-Sherwin@users.noreply.github.com>
2024-10-20 09:42:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def predict():
         return jsonify({"error": "An unexpected error occurred."}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0')
